533ccf7fd318eb83dbfccda58d887c8cdf29aa10
Bachitter <143877686+bachiitter@users.noreply.github.com>
2026-01-20 15:58:13 -0800

    add worker template

--- packages/lumier/src/cli/lib/template.ts
+++ packages/lumier/src/cli/lib/template.ts
@@ -1,4 +1,4 @@
-// Config Templates
+// Config Template
 export const CONFIG_TEMPLATE = (name: string) => `import { $config, Worker } from "lumier";
 
 export default $config({
@@ -12,4 +12,14 @@ export default $config({
     return {};
   },
 });
+`;
+
+// Worker Template
+export const WORKER_TEMPLATE = `import { env } from 'cloudflare:workers';
+
+export default {
+  async fetch(request: Request): Promise<Response> {
+    return new Response(\`Hello from \${env.STAGE}!\`);
+  },
+};
 `;
